51e0bec11d91f03802ab76922eda5656179f7a7b
Jonathan-rumpe <nuddelsoop@gmail.com>
2024-03-21 15:27:21 +0100

    tist

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
 const handlekruv = [];
 
+const produker = [
+    { "id": "1", "name": "GamingPc", "PRICE": 18000 },
+    { "id": "2", "name": "GamingKeyboard", "PRICE": 1000 },
+    { "id": "3", "name": "GamingMouse", "PRICE": 300 },
+    { "id": "4", "name": "GamingLaptop", "PRICE": 12000 }
+];
 
+function addItem(itemId) {
 
-function tisstest(gogogaga) {
-    document.getElementById("handle").innerHTML = ""
-    handlekruv.push(gogogaga)
-    for (let i = 0; i < handlekruv.length; i++) {
-        document.getElementById("handle").innerHTML += handlekruv[i] + "<br>"
-    }    
+    for (let produkt = 0; produkt < produker.length; produkt++) {
+
+        if (itemId == produker[produkt].id) {
+            document.getElementById("handle").innerHTML = "" //clear handekruv
+            handlekruv.push(produker[produkt]) // adder item to cart
+
+            for (let i = 0; i < handlekruv.length; i++) { // Går gjennom cart length og lager en ny line får hver av verdi
+                document.getElementById("handle").innerHTML += handlekruv[i].name + " " + handlekruv[i].PRICE + "<br>" // Cart line
+            }
+        }
+    }
 }
 console.log("400kr for 5g hasj")
+
