3d65e89a587b951ff521d6d30a3c6309eeff37ee
bossyobro <156893882+bossyobro@users.noreply.github.com>
2024-06-03 09:57:51 +0200

    Cook

--- Clothing/script.js
+++ Clothing/script.js
@@ -17,3 +17,37 @@ function filterItems(category) {
         });
     }
 }
+
+if (window.location.pathname.includes('index.html')) {
+    const navbar = document.querySelector('.Header');
+    const logo = document.getElementById('LogoKom');
+    const cartIcon = document.getElementById('cart');
+    const cartD = document.getElementById('cartD')
+    const MainShop = document.getElementById('MainShopID')
+
+    if (navbar && logo && cartIcon) {
+        navbar.classList.add('Transparent');
+        logo.src = 'bilder/clKomplitoLogoSvart.svg';
+        cartIcon.src = 'bilder/ShoppingBagNoFillSvart.svg';
+        cartD.classList.add('rightNavCartT')
+        MainShop.src = 'bilder/MainShoppingIconSvart.svg'
+
+        window.addEventListener('scroll', () => {
+            if (window.scrollY > 75) {
+                navbar.classList.remove('Transparent');
+                logo.src = 'bilder/clKomplitoLogo.svg';
+                cartIcon.src = 'bilder/ShoppingBagNoFill.svg';
+                MainShop.src = 'bilder/MainShoppingIcon.svg'
+                cartD.classList.remove('rightNavCartT')
+            } else {
+                navbar.classList.add('Transparent');
+                logo.src = 'bilder/clKomplitoLogoSvart.svg';
+                cartIcon.src = 'bilder/ShoppingBagNoFillSvart.svg';
+                MainShop.src = 'bilder/MainShoppingIconSvart.svg'
+                cartD.classList.add('rightNavCartT')
+            }
+        });
+    } else {
+        console.error('One or more elements not found');
+    }
+}
